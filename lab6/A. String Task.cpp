4c64a990cd9c003f3de2ceaa06b9e2f2cd2a2e31
Jannatul Ferdous <109688765+twinkle178@users.noreply.github.com>
2022-10-18 22:29:07 -0700

    Add files via upload

---
+++ lab6/A. String Task.cpp
@@ -0,0 +1,31 @@
+
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    string s,ch;
+    cin>>s;
+
+
+    for(int i=0;i<s.length();i++)
+    {
+
+       if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'||s[i]=='y'||s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U'||s[i]=='Y')
+       {
+           continue;
+       }
+       else
+       {
+           ch+='.';
+            ch+=tolower(s[i]);
+       }
+
+
+
+
+    }
+    cout<<ch<<endl;
+
+
+}
